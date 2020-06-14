31aae5c5a0a35b7aff8f830d7a250a443169298f
Raphael Jacob <r.jacob2002@gmail.com>
2020-06-14 02:59:37 +0200

    Add Output
    Exclude output in gitignore

---
+++ internal/output/output.go
@@ -0,0 +1,40 @@
+package output
+
+import (
+	"sync"
+
+	"github.com/ski7777/gosudoku/package/grid"
+)
+
+type Output struct {
+	outputters []Outputter
+	lock       sync.Mutex
+	count      int
+	limit      int
+	limitcall  func()
+}
+
+func (o *Output) RegisterOutputter(op Outputter) {
+	o.outputters = append(o.outputters, op)
+}
+
+func (o *Output) Output(g *grid.Grid) {
+	o.lock.Lock()
+	defer o.lock.Unlock()
+	if o.count == o.limit+1 {
+		defer func() { go o.limitcall() }()
+		return
+	}
+	for _, op := range o.outputters {
+		op.Output(g, o.count)
+	}
+	o.count++
+}
+
+func NewOutput(limit int, limitcall func()) *Output {
+	o := new(Output)
+	o.limit = limit
+	o.limitcall = limitcall
+	o.count = 1
+	return o
+}
---
+++ internal/output/outputter.go
@@ -0,0 +1,7 @@
+package output
+
+import "github.com/ski7777/gosudoku/package/grid"
+
+type Outputter interface {
+	Output(*grid.Grid, int)
+}
